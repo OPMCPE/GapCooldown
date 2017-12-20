b6eee14ce0019b117345a3b1a3af19cb00f5eee0
BigDGucci <darkkz6503@gmail.com>
2017-12-20 12:45:15 -0600

    Update Main.php

--- src/BigDGucci/GapCooldown/Main.php
+++ src/BigDGucci/GapCooldown/Main.php
@@ -1,1 +1,35 @@
 <?php
+
+namespace BigDGucci/GapCooldown;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\player\PlayerItemConsumeEvent;
+
+class Main extends PluginBase implements Listener {
+    
+  private $cooldownArray = array();
+  }
+    
+    public function onEnable() {
+        $this->getLogger()->info("GapCooldown By BigDGucci Enabled!");
+    }
+  
+    public function onDisable() {
+        $this->getLogger()->info("GapCooldown By BigDGucci Disabled!");
+    }  
+
+    public function onConsume(PlayerItemConsumeEvent $event);
+        if($event->getItem()->getId() === Item::ENCHANTED_GOLDEN_APPLE) {
+        //using isset may be unneccesary.
+           if(isset($this->cooldownArray[$event->getPlayer()->getName()]) && $this->cooldownArray[$event->getPlayer()->getName()] - time() < 60) {
+           //Player ate a Golden Apple within the last 60 seconds
+           $event->setCancelled();
+           } else {
+           //There is no cooldown for the player.
+           $this->cooldownArray[$event->getPlayer()->getName()] = time();  
+           }
+        }
+    }
+}
+      
